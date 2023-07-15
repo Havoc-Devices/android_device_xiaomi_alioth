490eedc0e2fe9bfa83a0dca6ca8ead4fd9ddafac
Anushek Prasal <anushekprasal@gmail.com>
2023-07-15 16:51:19 +0530

    alioth: Init Havoc...
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

--- lineage_alioth.mk
+++ lineage_alioth.mk
@@ -23,3 +23,5 @@ PRODUCT_MODEL := POCO F3
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 BUILD_FINGERPRINT := POCO/alioth_global/alioth:13/TKQ1.220829.002/V14.0.4.0.TKHMIXM:user/release-keys
+
+HAVOC_BUILD_TYPE := Official
